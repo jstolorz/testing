2a9e84ee43d230cab10294eed8e894d3afd22b93
Janusz <31537161+jstolorz@users.noreply.github.com>
2018-03-23 11:07:01 +0100

    Update hitcount.py

--- src/hitcount.py
+++ src/hitcount.py
@@ -2,8 +2,8 @@ import redis
 
 r = redis.StrictRedis(host='0.0.0.0', port=6379, db=0)
 
-def ahit(key):
+def hit(key):
     r.incr(key)
 
-def agetHit(key):
+def getHit(key):
     return (r.get(key))
